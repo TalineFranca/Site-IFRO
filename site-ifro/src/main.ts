fa0b211edb637cd76ff83a9bb85206db5deefebe
Taline Rodrigues <talinefranca32@gmail.com>
2025-07-21 17:44:47 -0400

    finalizado

--- site-ifro/src/main.ts
+++ site-ifro/src/main.ts
@@ -1,18 +1,15 @@
-import './style.css';
+import './style.css'
 
 const toggle = document.getElementById('menuToggle') as HTMLElement;
 const menu = document.getElementById('menu') as HTMLElement;
-const menuIcon = toggle.querySelector('.menu-icon') as HTMLImageElement;
-const closeIcon = toggle.querySelector('.close-icon') as HTMLImageElement;
+const menuIcon = toggle.querySelector('.icone-menu') as HTMLImageElement;
+const closeIcon = toggle.querySelector('.icone-fechar') as HTMLImageElement;
 
-// Inicialmente exibe apenas o ícone de menu
 menuIcon.style.display = 'block';
 closeIcon.style.display = 'none';
 
 toggle.addEventListener('click', () => {
   const isOpen = menu.classList.toggle('open');
-
-  // Alterna ícones
   menuIcon.style.display = isOpen ? 'none' : 'block';
   closeIcon.style.display = isOpen ? 'block' : 'none';
 });
